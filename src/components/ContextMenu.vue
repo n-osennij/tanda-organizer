86f2c33fb6c4b327cf2fa18f83c27bf3e31e3bb0
n-osennij <n.osennij@gmail.com>
2024-10-08 00:02:15 +0300

    menu prepare

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -19,6 +19,12 @@
           {{ index + 1 }}
         </button>
       </div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li></li>
+      </ul>
     </div>
   </div>
 </template>
